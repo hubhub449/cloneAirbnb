ad7cd930124a3e9b2e0f6320187fde3f15c0900b
gauthier HUBERT <gauthier.hubert@ehl.ch>
2019-08-18 16:09:05 +0200

    cloneAirbnb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :path => '',
+                     :path_names => { :sign_in => 'login', :sign_out => 'logout', :edit => 'profil' },
+                     :controllers => { :registrations => 'registrations',
+                                       :confirmations => 'confirmations' }
+  root 'pages#home'
+
+  resources :users, only: [ :show ]
+  resources :rooms, path: 'annonces' do
+    resources :reservations, only: [ :create ]
+    resources :reviews, only: [ :create, :destroy ]
+  end
+  resources :photos
+
+  resources :conversations, only: [ :index, :create ] do
+    resources :messages, only: [ :index, :create ]
+  end
+
+  get '/preload' => 'reservations#preload'
+  get '/preview' => 'reservations#preview'
+  get 'your_trips' => 'reservations#your_trips', path: 'mes_voyages'
+  get 'your_reservations' => 'reservations#your_reservations', path: 'mes_reservations'
+  get '/search' => 'pages#search'
 end
