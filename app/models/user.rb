0b2093d5937f3e4860ee3f0c02c894cde5924b1c
gauthier HUBERT <gauthier.hubert@ehl.ch>
2019-08-18 18:16:50 +0200

    ee

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :fullname, presence:true, length: { maximum: 50 }
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_many :rooms
+  has_many :reservations
+  has_many :reviews
+end
